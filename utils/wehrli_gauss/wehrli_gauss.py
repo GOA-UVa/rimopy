7c0a8ddc1228e61aa384b60f5c9490eae4a670c7
Javier Gatón Herguedas <gaton@goa.uva.es>
2022-02-06 12:41:39 +0100

    utils/wehrli_gauss::_gaussian_filter_list asks for standard deviation as a parameter
    Former-commit-id: 825d1f8e711232d145e5fe66c12a960751a5366f

--- utils/wehrli_gauss/wehrli_gauss.py
+++ utils/wehrli_gauss/wehrli_gauss.py
@@ -41,20 +41,22 @@ def _interpolate_list(f: interp1d, minimum: float, maximum: float, step: float) 
         l.append(f(i).item())
     return l
 
-def _gaussian_filter_list(l: List[float]) -> List[float]:
+def _gaussian_filter_list(l: List[float], sigma: np.ScalarType) -> List[float]:
     """Gaussian filters the list
 
     Parameters:
     -----------
     l : list of float
         List of float values that will be filtered.
+    sigma : scalar
+        standard deviation for Gaussian kernel
 
     Returns:
     --------
     list of float
         Filtered list of values.
     """
-    return ndimage.gaussian_filter1d(np.float_(l), 1)
+    return ndimage.gaussian_filter1d(np.float_(l), sigma)
 
 def _generate_new_list(data: List[List[float]]) -> List[List[float]]:
     """Generates the new interpolated and filtered list from the original list.
@@ -76,8 +78,9 @@ def _generate_new_list(data: List[List[float]]) -> List[List[float]]:
     step = 1
     l1 = _interpolate_list(f1, minimum, maximum, step)
     l2 = _interpolate_list(f2, minimum, maximum, step)
-    l1 = _gaussian_filter_list(l1)
-    l2 = _gaussian_filter_list(l2)
+    sigma = 1
+    l1 = _gaussian_filter_list(l1, sigma)
+    l2 = _gaussian_filter_list(l2, sigma)
 
     new_data = []
     index = 0
